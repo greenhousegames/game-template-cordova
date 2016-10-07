1c407715e14d54b51ab019191ee8857382daf65d
Brian Soumakian <bsoumakian@gmail.com>
2016-10-07 00:42:22 -0700

    update dependencies

--- app/app.js
+++ app/app.js
@@ -1,18 +1,12 @@
 import fs from 'fastclick';
 import firebase from 'firebase';
 import Game from '@greenhousegames/game-template';
-import '@greenhousegames/firebase-game-storage';
 import '@greenhousegames/greenhouse-phaser-plugin';
+import config from './config';
 
 fs.FastClick.attach(document.body);
 
-const firebaseInst = firebase.initializeApp({
-  apiKey: 'AIzaSyCqcbh9XmZZBahTDxmj4GXbwQny9H9wHdo',
-  authDomain: 'greenhouse-games-test.firebaseapp.com',
-  databaseURL: 'https://greenhouse-games-test.firebaseio.com',
-  storageBucket: '',
-  messagingSenderId: '756331628096'
-});
+const firebaseInst = firebase.initializeApp(config);
 
 const game = new Game({
   firebase: firebaseInst,
